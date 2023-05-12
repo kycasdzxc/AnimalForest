1757a1b81042afab9f179242c651f22a522c37a4
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2023-05-12 20:52:02 +0900

    Spring Boot 프로젝트 생성

---
+++ src/main/java/kr/co/yermi/animalforest/AnimalForestApplication.java
@@ -0,0 +1,13 @@
+package kr.co.yermi.animalforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalForestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalForestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/yermi/animalforest/AnimalForestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.yermi.animalforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalForestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
